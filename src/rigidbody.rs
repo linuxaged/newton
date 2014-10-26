b707d02c5669801dd45b2c9166b564cfab6f20b7
Tracy Ma <1991md@gmail.com>
2014-10-26 13:58:00 +0800

    add rigidbody

---
+++ src/rigidbody.rs
@@ -0,0 +1,13 @@
+struct Rigidbody {
+    inverseMass: f32,
+    position: Vector3,
+    oreitation: Quaternion,
+    velocity: Vector3,
+    transformMatrix: Matrix4
+}
+
+impl Rigidbody {
+    fn calculateDerivedData() {
+
+    }
+}
