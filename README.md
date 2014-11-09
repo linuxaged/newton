b2503c37ae4dc9dc5a2fe0e106c140a0d2349e07
Tracy Ma <1991md@gmail.com>
2014-11-09 22:41:37 +0800

    update readme

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ physics engine in rust
 
 TODO
 ======
-    update force
+    gen the gl.rs
+        export DYLD_LIBRARY_PATH=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/
+        ./bindgen /System/Library/Frameworks/OpenGL.framework/Headers/gl.h -o ~/Downloads/gl.rs -framework-link OpenGL
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![feature(phase)]
 #[phase(plugin)] extern crate bindgen;
+extern crate libc;
 
 #[allow(dead_code, uppercase_variables, non_camel_case_types)]
 mod gl_bindings {
-    bindgen!("/System/Library/Frameworks/OpenGL.framework/Headers/gl.h", match="gl.h", link_framework="OpenGL")
+    bindgen!("/System/Library/Frameworks/OpenGL.framework/Headers/gl.h", link_framework="OpenGL", emit_builtins=true)
 }
