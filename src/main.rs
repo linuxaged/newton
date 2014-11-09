ac0d181dd46450c1a16b338ca79e8f58ce4efa78
Tracy Ma <1991md@gmail.com>
2014-11-09 20:56:05 +0800

    fix Cargo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@
 #[phase(plugin)] extern crate bindgen;
 
 #[allow(dead_code, uppercase_variables, non_camel_case_types)]
-mod mysql_bindings {
-    bindgen!("/usr/include/mysql/mysql.h", match="mysql.h", link="mysql")
+mod gl_bindings {
+    bindgen!("/System/Library/Frameworks/OpenGL.framework/Headers/gl.h", match="gl.h", link_framework="OpenGL")
 }
