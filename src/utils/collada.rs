b0dc73b05b1963e6ea051497db31ae3e29720ae3
Tracy Ma <madi@chukong-inc.com>
2014-12-04 11:41:58 +0800

    partly collada finish

--- src/utils/collada.rs
+++ src/utils/collada.rs
@@ -2,4 +2,32 @@ pub mod collada {
 	// parse .dae file
 	// <mesh> <source id = "">  </source> </mesh>
 	// 需要先解析 source, 然后是 verteies, triangles
+	enum ParseType {
+	    vertex,
+	    triangle,
+	}
+
+	struct Collada {
+	    content: String,
+	    tag: str,
+	}
+
+	impl Collada {
+		fn new(path: str) -> Collada {
+			let content = File::open(Path::new(path).read_to_string().unwrap());
+			Collada{content: content, tag: ""}
+		}
+		fn parse_vertex(&self) -> [f32..] {
+	        let path = Path::new("/tmp/data.txt");
+			let raw_string = File::open(&path).read_to_string().unwrap();
+			let result = BoyerMoore::new(raw_string.as_slice(), "SIMPLE").search();
+		}
+		pub fn parse(&self, t: ParseType) -> [f32..] {
+			match t {
+				self.tag = ""
+				ParseType::vertex => parse_vertex()
+			}
+		}
+	}
+	
 }
