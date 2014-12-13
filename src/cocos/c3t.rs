56d7cc92337af5a44960453ebfac6a50f0dfae92
Tracy Ma <1991md@gmail.com>
2014-12-13 16:39:54 +0800

    add c3t format parse

---
+++ src/cocos/c3t.rs
@@ -0,0 +1,20 @@
+extern crate "gfx_gl" as gl;
+
+struct MeshVertexAttribs {
+    size: GLint,
+    t: gl::types::GLenum,
+    vertexAttrib: int,
+    attribSizeBytes: int,
+}
+
+struct MeshData {
+    let IndexArray = Vec<u16>,
+    vertex: Vec<f32>,
+    vertexSizeInFloat: int,
+    subMeshIndices: Vec<IndexArray>,
+    subMeshIds: Vec<String>,
+    subMeshAABB: Vec<AABB>,
+    numIndex: int,
+    atttibs: Vec<MeshVertexAttribs>,
+    attribCount: int
+}
