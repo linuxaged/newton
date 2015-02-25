af210f965cff92a79789c03211f10e255cf34acb
Tracy Ma <1991md@gmail.com>
2015-02-25 21:38:08 +0800

    fix compile error

--- src/math/quaternion.rs
+++ src/math/quaternion.rs
@@ -1,38 +1,41 @@
-mod quaternion {
+use std::ops::Mul;
+use std::num::Float;
 
-    pub struct Quaternion {
-        r: f32,
-        i: f32,
-        j: f32,
-        k: f32,
-        data: [f32; 4]
+pub struct Quaternion {
+    pub r: f32,
+    pub i: f32,
+    pub j: f32,
+    pub k: f32,
+    pub data: [f32; 4]
+}
+
+impl Quaternion {
+    pub fn new(r: f32, i: f32, j: f32, k: f32) -> Quaternion {
+        Quaternion { r: r, i: i, j: j, k: k, data: [0f32; 4]}
     }
-
-    impl Quaternion {
-        fn new(r: f32, i: f32, j: f32, k: f32) -> Quaternion {
-            Quaternion { r: r, i: i, j: j, k: k, data: [0, ..4]}
+    pub fn normalize(&mut self) {
+        let mut d = self.r * self.r + self.i * self.i   + self.j * self.j + self.k * self.k;
+        if (d == 0f32) {
+            self.r = 1.0f32;
+            return;
         }
-        fn normalize(&self) -> ret {
-            let d = self.r * self.r + self.i * self.i   + self.j * self.j + self.k * self.k;
-            if (d == 0) {
-                r =1;
-                return;
-            }
-            d = 1.0 / real_sqrt(d);
-            self.r *= d;
-            self.i *= d;
-            self.j *= d;
-            self.k *= d;
+        d = d.rsqrt();
+        self.r *= d;
+        self.i *= d;
+        self.j *= d;
+        self.k *= d;
+    }
+}
+
+impl Mul for Quaternion {
+    type Output = Quaternion;
+    fn mul(self, other: Quaternion) -> Quaternion {
+        Quaternion {
+            r: self.r * other.r - self.i* other.i - self.j* other.j - self.k* other.k,
+            i: self.r * other.i + self.i* other.r + self.j* other.k - self.k* other.j,
+            j: self.r * other.j + self.j* other.r + self.k* other.i - self.i* other.k,
+            k: self.r * other.k + self.k* other.r + self.i* other.j - self.j* other.i,
+            data: self.data
         }
     }
-
-    impl Mul<Quaternion, Quaternion> for Quaternion {
-        fn mul(&self, other: &Quaternion) -> Quaternion {
-            self.r = self.r * other.r - self.i* other.i - self.j* other.j - self.k* other.k;
-            self.i = self.r * other.i + self.i* other.r + self.j* other.k - self.k* other.j;
-            self.j = self.r * other.j + self.j* other.r + self.k* other.i - self.i* other.k;
-            self.k = self.r * other.k + self.k* other.r + self.i* other.j - self.j* other.i;
-        }
-    }
-
 }
--- src/math/vector3.rs
+++ src/math/vector3.rs
@@ -1,52 +1,54 @@
-pub use std::fmt;
+use std::fmt;
+use std::ops::{Add, Sub, Mul};
 
-mod vector3 {
-
-    pub struct Vector3 {
-        x: f32,
-        y: f32,
-        z: f32
-    }
-
-    impl Vector3 {
-        fn new(x: f32, y: f32, z: f32) -> Vector3 {
-            Vector3{ x: x, y: y, z: z }
-        }
-        fn magnitude(&self) -> f32 {
-            self.x * self.x + self.y * self.y + self.z * self.z
-        }
-        fn add_scaled_vector(&mut self, vector: &Vector3, scale: f32) {
-            self.x += vector.x * scale;
-            self.y += vector.y * scale;
-            self.z += vector.z * scale;
-        }
-    }
-
-    impl Add<Vector3, Vector3> for Vector3 {
-        fn add(&self, other: &Vector3) -> Vector3 {
-            Vector3 {x: self.x + other.x, y: self.y + other.y, z: self.z + other.z}
-        }
-    }
-
-    impl Sub<Vector3, Vector3> for Vector3 {
-        fn sub(&self, other: &Vector3) -> Vector3 {
-            Vector3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
-        }
-    }
-
-    impl Mul<Vector3, Vector3> for Vector3 {
-        fn mul(&self, other: &Vector3) -> Vector3 {
-            Vector3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
-        }
-    }
-
-    impl fmt::Show for Vector3 {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "({}, {}, {})", self.x, self.y, self.z)
-        }
-    }
-
+pub struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
 }
+
+impl Vector3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vector3 {
+        Vector3{ x: x, y: y, z: z }
+    }
+    pub fn magnitude(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+    pub fn add_scaled_vector(&mut self, vector: &Vector3, scale: f32) {
+        self.x += vector.x * scale;
+        self.y += vector.y * scale;
+        self.z += vector.z * scale;
+    }
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+    fn add(self, other: Vector3) -> Vector3 {
+        Vector3 {x: self.x + other.x, y: self.y + other.y, z: self.z + other.z}
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+    fn sub(self, other: Vector3) -> Vector3 {
+        Vector3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
+    }
+}
+
+impl Mul for Vector3 {
+    type Output = Vector3;
+    fn mul(self, other: Vector3) -> Vector3 {
+        Vector3 {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
+    }
+}
+
+impl fmt::Show for Vector3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+
 
 #[test]
 fn test_mul() {
