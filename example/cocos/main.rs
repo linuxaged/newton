7b8b8ef774ab8f002a778acfc3a86ae87e82534f
Tracy Ma <madi@chukong-inc.com>
2015-02-25 17:07:20 +0800

    update to the new fs api

--- example/cocos/main.rs
+++ example/cocos/main.rs
@@ -1,13 +1,14 @@
 extern crate serialize;
 use serialize::json;
-use std::io::File;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let path = Path::new("orc.c3t");
-    let raw_string = File::open(&path).read_to_string().unwrap();
+    let mut raw_string = String::new();
+    File::open("orc.c3t").unwrap().read_to_string(&mut raw_string);
 
-    match json::from_str(raw_string) {
-        Ok(json) => println!("{}", json.find("vertion").unwrap()),
+    match json::from_str(&raw_string) {
+        Ok(json) => println!("{}", json.find("version").unwrap()),
         Err(err) => println!("{}", err),
     }
 }
