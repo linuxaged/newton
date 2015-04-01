4db9158561d86d8f1b2bfea03735ec7a6b4c9bec
Tracy Ma <madi@chukong-inc.com>
2015-04-01 08:45:57 +0800

    add todo

--- src/net/net.rs
+++ src/net/net.rs
@@ -1,5 +1,8 @@
-// todo: rudp library
-
+// 1.Server -> Start (update waiting for data)
+// 2.Client -> Connect to Server
+// 3.Server -> Send snapshot to all clients
+// snapshot 的计算和渲染：http://gamedev.stackexchange.com/questions/87553/how-would-a-game-state-snapshot-system-be-implemented-for-networked-real-time-ga
+// Quake3 相关 snapshot 参考：https://github.com/id-Software/Quake-III-Arena/blob/dbe4ddb10315479fc00086f08e25d968b4b43c49/code/server/sv_snapshot.c
 use std::net::UdpSocket;
 
 fn main() {
