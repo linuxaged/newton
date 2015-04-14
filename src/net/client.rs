c29950a122a7e972947e23689535e82f4ac41a74
马迪 <madi@chukong-inc.com>
2015-04-14 11:48:50 +0800

    add client and server

---
+++ src/net/client.rs
@@ -0,0 +1,16 @@
+use std::net::{SocketAddrV4, Ipv4Addr};
+use std::net::UdpSocket;
+fn main() {
+    let mut socket = (UdpSocket::bind("127.0.0.1:4445")).unwrap();
+
+    let mut buf = [0; 10];
+    let (amt, src) = (socket.recv_from(&mut buf)).unwrap();
+
+    // Send a reply to the socket we received data from
+    let buf = &mut buf[..amt];
+    buf.reverse();
+    let serverIp = SocketAddrV4.new(Ipv4Addr::new(127,0,0,1), 4444);
+    (socket.send_to(buf, &serverIp)).unwrap();
+
+    drop(socket); // close the socket
+}
---
+++ src/net/server.rs
@@ -0,0 +1,15 @@
+use std::net::UdpSocket;
+
+fn main() {
+    let mut socket = (UdpSocket::bind("127.0.0.1:4444")).unwrap();
+
+    let mut buf = [0; 10];
+    let (amt, src) = (socket.recv_from(&mut buf)).unwrap();
+
+    // Send a reply to the socket we received data from
+    let buf = &mut buf[..amt];
+    buf.reverse();
+    (socket.send_to(buf, &src)).unwrap();
+
+    drop(socket); // close the socket
+}
