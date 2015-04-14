3f381739b0cf8174dfa42494edc3056f366e76b6
马迪 <madi@chukong-inc.com>
2015-04-14 14:47:08 +0800

    server in loop

--- src/net/client.rs
+++ src/net/client.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut socket = (UdpSocket::bind("127.0.0.1:4445")).unwrap();
 
     let serverIp = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 4444);
-    (socket.send_to(b"tracy", &serverIp)).unwrap();
+    (socket.send_to(b"tracytracy", &serverIp)).unwrap();
 
     drop(socket); // close the socket
 }
--- src/net/server.rs
+++ src/net/server.rs
@@ -2,16 +2,20 @@ use std::net::UdpSocket;
 use std::str::from_utf8;
 
 fn main() {
-    let mut socket = (UdpSocket::bind("127.0.0.1:4444")).unwrap();
+    let socket = (UdpSocket::bind("127.0.0.1:4444")).unwrap();
 
     let mut buf = [0; 10];
-    let (amt, src) = (socket.recv_from(&mut buf)).unwrap();
-    if (amt > 0) {
-    	println!("{}", from_utf8(&buf).unwrap());
-    }
-    let buf = &mut buf[..amt];
-    buf.reverse();
-    (socket.send_to(buf, &src)).unwrap();
 
-    drop(socket); // close the socket
+    loop {
+    	let (amt, src) = (socket.recv_from(&mut buf)).unwrap();
+	    if amt > 0 {
+	    	println!("receive {} bytes: {}", amt, from_utf8(&buf).unwrap());
+	    	let buf = &mut buf[..amt];
+		    buf.reverse();
+		    (socket.send_to(buf, &src)).unwrap();
+	    }
+	    
+    }
+    
+    // drop(socket); // close the socket
 }
