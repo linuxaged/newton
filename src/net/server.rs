cc75373427e5fa90d489b0d61e21fda2d22ac908
马迪 <madi@chukong-inc.com>
2015-04-14 14:30:56 +0800

    success send string

--- src/net/server.rs
+++ src/net/server.rs
@@ -1,12 +1,14 @@
 use std::net::UdpSocket;
+use std::str::from_utf8;
 
 fn main() {
     let mut socket = (UdpSocket::bind("127.0.0.1:4444")).unwrap();
 
     let mut buf = [0; 10];
     let (amt, src) = (socket.recv_from(&mut buf)).unwrap();
-
-    // Send a reply to the socket we received data from
+    if (amt > 0) {
+    	println!("{}", from_utf8(&buf).unwrap());
+    }
     let buf = &mut buf[..amt];
     buf.reverse();
     (socket.send_to(buf, &src)).unwrap();
