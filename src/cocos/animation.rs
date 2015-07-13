3f884dc17c1ac3d9a57f393cb4848c7a1c3beb61
Tracy Ma <madi@chukong-inc.com>
2015-07-13 19:56:34 +0800

    animation need a update

--- src/cocos/animation.rs
+++ src/cocos/animation.rs
@@ -13,6 +13,8 @@ pub struct BoneBlendState {
     localScale: vector3::Vector3,
     weight: f32
 }
+
+/// static binding pose tree of model
 #[derive(Clone, Serialize, Deserialize, Display)]
 pub struct Node {
     pub id: String,
@@ -21,10 +23,27 @@ pub struct Node {
     pub children: Option<Vec<Node>>
 }
 
+/// visit the nodes and store 
+impl Node {
+    pub fn visit(&self) {
+
+    }
+}
+
+/// a key frame of animation
 #[derive(Clone, Serialize, Deserialize, Display, Debug)]
 pub struct KeyFrame {
     keytime: f32,
     rotation: [f32; 4],
     scale: [f32; 3],
     translation: [f32; 3]
+}
+
+pub struct Animation {
+}
+
+impl Animation {
+    pub fn update(&self,t: f32) {
+        
+    }
 }
