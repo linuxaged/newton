ad4e051fcc9fda3e4e1cd33053c1f5841080cb0c
马迪 <madi@chukong-inc.com>
2015-08-11 18:00:43 +0800

    add test to Quaternion

--- src/math/quaternion.rs
+++ src/math/quaternion.rs
@@ -2,6 +2,7 @@ use std::ops::Mul;
 use std::f32;
 use std::cmp;
 
+#[derive(Debug, Copy, Clone)]
 pub struct Quaternion {
     pub r: f32,
     pub i: f32,
@@ -13,6 +14,7 @@ impl Quaternion {
     pub fn new(r: f32, i: f32, j: f32, k: f32) -> Quaternion {
         Quaternion { r: r, i: i, j: j, k: k}
     }
+
     pub fn normalize(&mut self) {
         let mut d = self.r * self.r + self.i * self.i   + self.j * self.j + self.k * self.k;
         if (d == 0f32) {
@@ -26,28 +28,25 @@ impl Quaternion {
         self.k *= d;
     }
     pub fn slerp(from: &Quaternion, to: &Quaternion, t: f32, result: &mut Quaternion) {
-        let fCosine: f32;
+        let mut fCosine: f32;
         let fAngle: f32;
         let A: f32;
         let B: f32;
 
         // Parameter checking
         if (t<0.0f32 || t>1.0f32) {
-            result.r = 0;
-            result.i = 0;
-            result.j = 0;
-            result.k = 1;
+            result.r = 0.0;
+            result.i = 0.0;
+            result.j = 0.0;
+            result.k = 1.0;
             return;
         }
 
         // Find sine of Angle between Quaternion A and B (dot product between quaternion A and B)
         fCosine = from.r*to.r + from.i*to.i + from.j*to.j + from.k*to.k;
 
-        if (fCosine < 0)
+        if (fCosine < 0.0)
         {
-            let qi: Quaternion;
-
-            
             // <http://www.magic-software.com/Documentation/Quaternions.pdf>
 
             // "It is important to note that the quaternions q and -q represent
@@ -59,23 +58,22 @@ impl Quaternion {
             // customary to choose the sign... on q1 so that... the angle
             // between q0 and q1 is acute. This choice avoids extra
             // spinning caused by the interpolated rotations."
-            
-            qi.r = -to.r;
-            qi.i = -to.i;
-            qi.j = -to.j;
-            qi.k = -to.k;
-            
-            Quaternion::slerp(from, qi, t, result);
+            let mut qi = Quaternion::new(-to.r, -to.i, -to.j, -to.k);
+
+            Quaternion::slerp(from, &qi, t, result);
             return;
         }
 
-        fCosine = cmp::min(fCosine, 1.0f32);
+        fCosine = fCosine.min(1.0);
         fAngle = fCosine.acos();
 
         // Avoid a division by zero
         if (fAngle==0.0f32)
         {
-            result = from;
+            result.i = from.i;
+            result.j = from.j;
+            result.k = from.k;
+            result.r = from.r;
             return;
         }
 
@@ -90,7 +88,7 @@ impl Quaternion {
         result.r = A * from.r + B * to.r;
 
         // Normalise result
-        normalize(result);
+        result.normalize();
     }
 }
 
@@ -104,4 +102,18 @@ impl Mul for Quaternion {
             k: self.r * other.k + self.k* other.r + self.i* other.j - self.j* other.i
         }
     }
+}
+
+#[test]
+fn test_norm() {
+    let mut q0 = Quaternion::new(0.1, 0.2, 0.3, 0.4);
+    q0.normalize();
+    println!("norm:{:?}", q0);
+
+    let q1 = Quaternion::new(0.1, 0.2, 0.3, 0.4);
+    let q2 = Quaternion::new(0.1, 0.2, 0.3, 0.3);
+
+    Quaternion::slerp(&q1, &q2, 0.5, &mut q0);
+    println!("slerp:{:?}", q0);
+
 }
